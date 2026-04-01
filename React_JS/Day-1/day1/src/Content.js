e4c8b03ba9aa6d85084c862ac56c52077dba3851
PrajuMaharjan <prajumaharjan903@gmail.com>
2026-04-01 18:33:55 +0545

    Day 1

---
+++ React_JS/Day-1/day1/src/Content.js
@@ -0,0 +1,10 @@
+const Content=()=>{
+    return(
+        <main className="App-header">
+        <p>
+        Day 1 of Learning MERN Stack(Chapter 1:React JS)
+        </p>
+        </main>
+    )
+}
+export default Content;
