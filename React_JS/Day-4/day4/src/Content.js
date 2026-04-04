3244f13fb27762e857b0c3e67322fc9b68de0c5d
PrajuMaharjan <prajumaharjan903@gmail.com>
2026-04-04 19:02:18 +0545

    Day - 4 Lists and Keys

--- React_JS/Day-4/day4/src/Content.js
+++ React_JS/Day-4/day4/src/Content.js
@@ -1,30 +1,71 @@
 import { useState } from "react"; 
+import {FaTrashAlt} from 'react-icons/fa'; 
 
 const Content=()=>{
-    const [name,setName]=useState('praju');
-    const[count,setCount]=useState(0);
+    const names=[
+        {
+            id:1,
+            name:"John",
 
-    const handleClick=()=>{
-        if (name==="praju"){
-         setName("PRAJU");
-        }else{
-         setName("praju");
+        },
+        {
+            id:2,
+            name:"Arthur",
+        },
+        {
+            id:3,
+            name:"Susan",
+        },
+        {
+            id:4,
+            name:"Karen",
         }
-        setCount(count+1);
+    ];
+
+    const [name,setName]=useState(()=>{
+        const storedNames=localStorage.getItem("Names");
+        return storedNames ? JSON.parse(storedNames):names;
+    });
+    const handleDelete=(id)=>{
+        const updatedNames=name.filter(name=>name.id!==id);
+        setName(updatedNames);
+        localStorage.setItem("Names",JSON.stringify(updatedNames));
+    }
+
+    const handleAdd=()=>{
+        const missingNames=names.filter((person)=>!name.some((current)=>current.id===person.id)
+    );
+    if (!missingNames.length) return;
+
+    const updatedNames=[...name,missingNames[0]];
+    setName(updatedNames);
+    localStorage.setItem("Names",JSON.stringify(updatedNames));
     }
 
     return(
         <main className="App-header">
         <p >
-        Day 1 of Learning MERN Stack(Chapter 1:React JS)
+        Day 4 of Learning MERN Stack(Chapter 1:React JS)
         </p>
-        
-        <p >
-            Hello {name}!
-        </p>
-        <button onClick={handleClick}>Click me to capitalize/decapitalize your name</button>
-        <p>Button clicked {count} times.</p>
+         {name.length ? (
+            <ul>
+                {name.map((name)=>(
+                    <li className="name" key={name.id}>
+                        <label>Hello {name.name}!   </label>
+                        <FaTrashAlt 
+                            role="button"
+                            tabIndex="0"
+                            onClick={()=>handleDelete(name.id)}
+                        />
+                    </li>
+                ))}
+            </ul>
+         ) :(
+            <p>There are no names</p>
+         )}
+                    <button onClick={handleAdd}>Add Name</button>
         </main>
     )
 }
+
 export default Content;
