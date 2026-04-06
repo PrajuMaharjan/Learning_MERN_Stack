c8ed914d2c507767fdc03a612297bd3b3f9eb638
PrajuMaharjan <prajumaharjan903@gmail.com>
2026-04-06 21:39:20 +0545

    Day-6 MCQ

--- React_JS/Day-6/day6/src/Footer.js
+++
@@ -1,9 +0,0 @@
-const Footer=()=>{
-    const name="Praju Maharjan";
-    return(
-        <footer className="App-header">
-        <p>{name}</p>
-        </footer>
-    )
-}
-export default Footer;
